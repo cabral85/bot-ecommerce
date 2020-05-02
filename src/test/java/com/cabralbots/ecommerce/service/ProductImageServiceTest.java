8418a804b64b65a296a1fdfa69b4a52e3a7bd7d8
Denis <cabral85@gmail.com>
2020-05-02 14:33:42 -0300

    checkpoint - criação de estrutura para testes

---
+++ src/test/java/com/cabralbots/ecommerce/service/ProductImageServiceTest.java
@@ -0,0 +1,29 @@
+package com.cabralbots.ecommerce.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductImageServiceTest {
+
+    @Test
+    void saveProductImage() {
+        Assert.isTrue(true);
+    }
+
+    @Test
+    void getProductImage() {
+        Assert.isTrue(true);
+    }
+
+    @Test
+    void getProductImages() {
+        Assert.isTrue(true);
+    }
+
+    @Test
+    void deleteImage() {
+        Assert.isTrue(true);
+    }
+}
